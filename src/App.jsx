e637e0d9a2b10da487c9b32007f019628350fa2d
Dulan Waduge <dulanwaduge@192-168-1-179.tpgi.com.au>
2023-03-23 04:23:39 +1100

    created Dashboard component in /src/pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// Routes
+import Dashboard from "./pages/Dashboard";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+  {
+    path: "/about",
+    element: <h1>About</h1>,
+  },
+]);
+
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <RouterProvider router={router} />
+  </div>;
 }
 
 export default App;
