2edddf1e8913f898d50637ee22eaf62841948453
Dulan Waduge <dulanwaduge@192-168-1-179.tpgi.com.au>
2023-03-24 19:15:50 +1100

    creted Error page with buttons to navigate home or back -> For when error creating new user account

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -16,14 +16,13 @@ export async function dashboardAction({request}) {
   const data = await request.formData();
   const formData = Object.fromEntries(data)
 
-  try{
+  try {
     localStorage.setItem("userName", JSON.stringify(formData.userName))
-
     return toast.success(`Welcome, ${formData.userName} !`)
-  } catch (e) {
+  } 
+  catch (e) {
     throw new Error("There was a problem creating your account.")
   }
-  
 }
 
 const Dashboard = () => {
